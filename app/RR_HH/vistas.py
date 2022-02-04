40976f79fbf9d34da75567d65ba14acecc908117
Erick <erickfco1999m@gmail.com>
2022-02-04 16:03:10 -0600

    configurando privilegios

--- app/RR_HH/vistas.py
+++ app/RR_HH/vistas.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def directorio(request):
-    return render(request, 'RRHH/directorio.html')
+    if request.session.get("email"):
+        context = {
+            'privilegio': request.session.get("privilegio")
+        }
+        return render(request, 'RRHH/directorio.html', context)
+    else:
+        return redirect("/cerrar_sesion")
 
 def perfil_puesto(request):
-    return render(request, 'RRHH/perfil_puesto.html')
+    if request.session.get("email"):
+        context = {
+            'privilegio': request.session.get("privilegio")
+        }
+        return render(request, 'RRHH/perfil_puesto.html', context)
+    else:
+        return redirect("/cerrar_sesion")
