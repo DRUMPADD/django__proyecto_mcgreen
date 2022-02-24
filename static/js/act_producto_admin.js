fb5245ab8c1dc4389d61508ee24d37fa359eb274
Erick <erickfco1999m@gmail.com>
2022-02-23 20:05:37 -0600

    archivo por ocupar

---
+++ static/js/act_producto_admin.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('.producto').on('click', (e) => {
+        const element = $(this)[0].activeElement.parentElement.parentElement;
+        // console.log($(element).find("td").eq(5).text());
+        const cantidad = parseInt($(element).find("td").eq(3).text().split(" ")[1].replace(',', ''));
+        const precio = parseFloat($(element).find("td").eq(5).text().split(" ")[1].replace(',', ''));
+        // console.log(precio);
+        $("input[name='id_producto']").val($(element).find("th").text());
+        $("input[name='producto']").val($(element).find("td").eq(0).text());
+        $("input[name='descripcion']").val($(element).find("td").eq(1).text());
+        $("input[name='cantidad']").val(cantidad);
+        $("input[name='precio']").val(precio);
+        $("#BtnGuardar").val("Modificar").addClass("btn-success");
+        $("#BtnGuardar").removeClass("btn-primary");
+        if(!document.getElementById('cancelar')) {
+            $(this).val("Cancelar");
+            $("#form-pro").attr('action', "modificar_producto");
+        }
+
+        e.preventDefault();
+    });
+});
