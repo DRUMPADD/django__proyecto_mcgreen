116ea7093d3cc4566c33c0301ba1efc076d9ee62
Erick <erickfco1999m@gmail.com>
2022-02-23 23:12:51 -0600

    funciones de perfil

--- app/RR_HH/posts.py
+++ app/RR_HH/posts.py
@@ -20,6 +20,195 @@ def crear_perfil(request):
             cursor.close()
         return JsonResponse({"msg": mensaje, "msg_salida": mensaje_salida, "msg_error": mensaje_error, "puesto_creado": ""}, status=200)
 
+def funcion1(request):
+    if request.method == 'POST':
+        puesto = request.POST.get("puesto")
+        subor = request.POST.get("subordinacion")
+        superv = request.POST.get("supervision")
+        mensaje = ""
+        try:
+            cursor = connection.cursor()
+            cursor2 = connection.cursor()
+            if subor != None and superv == None:
+                cursor.callproc("AGREGAR_SUB_SUP", ["erick@sigssmac.com.mx", puesto, subor])
+                mensaje = cursor.fetchall()[0][0]
+                cursor.close()
+            elif subor == None and superv != None:
+                cursor2.callproc("AGREGAR_SUP", ["erick@sigssmac.com.mx", superv, puesto])
+                mensaje = cursor2.fetchall()[0][0]
+                cursor2.close()
+            else:
+                cursor.callproc("AGREGAR_SUB_SUP", ["erick@sigssmac.com.mx", puesto, subor])
+                mensaje = cursor.fetchall()[0][0]
+                cursor.close()
+                cursor2.callproc("AGREGAR_SUP", ["erick@sigssmac.com.mx", superv, puesto])
+                cursor2.close()
+            cursor.close()
+            cursor2.close()
+        except IntegrityError as e:
+            print(e.message)
+            return HttpResponse(e.message, status=500)
+        if mensaje == "RELACION DE JERARQUIA CREADA":
+            return JsonResponse({"msg": mensaje, "tipo_res": "success"}, status=200)
+        else:
+            return JsonResponse({"msg": mensaje, "tipo_res": "error"}, status=500)
+
+def funcion2(request):
+    if request.method == 'POST':
+        puesto = request.POST.get("puesto")
+        forma = request.POST.get("formacion")
+        escol = request.POST.get("escolaridad")
+        per_prof = request.POST.get("per_prof")
+        exper = request.POST.get("exper")
+        try:
+            cursor = connection.cursor()
+            cursor.callproc("RH_AGREGAR_PA_PR", ["erick@sigssmac.com.mx", puesto, forma, escol, per_prof, exper])
+            mensaje = cursor.fetchall()[0][0]
+        except IntegrityError as e:
+            print(e.message)
+            return HttpResponse(e.message, status=500)
+        finally:
+            cursor.close()
+
+        if mensaje == "PERFIL ACADEMICO Y/O PROFESIONAL AGREGADO CORRECTAMENTE":
+            return JsonResponse({"msg": mensaje, "tipo_res": "success"}, status=200)
+        else:
+            return JsonResponse({"msg": mensaje, "tipo_res": "error"}, status=500)
+
+def funcion3(request):
+    if request.method == 'POST':
+        puesto = request.POST.get("puesto")
+        funci = request.POST.get("funcion")
+        descrip_fun = request.POST.get("descrip_fun")
+        perio = request.POST.get("perio")
+        try:
+            cursor = connection.cursor()
+            cursor.callproc("RH_AGREGAR_FUNCION", ["erick@sigssmac.com.mx", puesto, funci, descrip_fun, perio])
+            mensaje = cursor.fetchall()[0][0]
+            cursor.close()
+        except IntegrityError as e:
+            print(e.message)
+            return HttpResponse(e.message, status=500)
+        if mensaje == "FUNCION AGREGADA CORRECTAMENTE":
+            return JsonResponse({"msg": mensaje, "tipo_res": "success"}, status=200)
+        else:
+            return JsonResponse({"msg": mensaje, "tipo_res": "error"}, status=500)
+
+def funcion4(request):
+    if request.method == 'POST':
+        puesto = request.POST.get("puesto")
+        sl_tipo_responsabilidad = request.POST.get("sl_tipo_responsabilidad")
+        descrip_resp = request.POST.get("descrip_resp")
+        try:
+            cursor = connection.cursor()
+            cursor.callproc("RH_AGREGAR_RESPONS", ["erick@sigssmac.com.mx", puesto, sl_tipo_responsabilidad, descrip_resp])
+            mensaje = cursor.fetchall()[0][0]
+            cursor.close()
+        except IntegrityError as e:
+            print(e.message)
+            return HttpResponse(e.message, status=500)
+        if mensaje == "RESPONSABILIDAD AGREGADA":
+            return JsonResponse({"msg": mensaje, "tipo_res": "success"}, status=200)
+        else:
+            return JsonResponse({"msg": mensaje, "tipo_res": "error"}, status=500)
+
+def funcion5(request):
+    if request.method == 'POST':
+        puesto = request.POST.get("puesto")
+        sl_competencia = request.POST.get("sl_competencia")
+        sl_dominio = request.POST.get("sl_dominio")
+        descrip_comp = request.POST.get("descrip_comp")
+        try:
+            cursor = connection.cursor()
+            cursor.callproc("RH_AGREGAR_COMP_GEN", ["erick@sigssmac.com.mx", puesto, sl_competencia, int(sl_dominio), descrip_comp])
+            mensaje = cursor.fetchall()[0][0]
+            cursor.close()
+        except IntegrityError as e:
+            print(e.message)
+            return HttpResponse(e.message, status=500)
+        if mensaje == "COMPETENCIA AGREGADA A PUESTO ACTUAL":
+            return JsonResponse({"msg": mensaje, "tipo_res": "success"}, status=200)
+        else:
+            return JsonResponse({"msg": mensaje, "tipo_res": "error"}, status=500)
+
+def funcion6(request):
+    if request.method == 'POST':
+        puesto = request.POST.get("puesto")
+        comp_tec = request.POST.get("comp_tec")
+        dom_tec = request.POST.get("sl_dominio")
+        desc_tec = request.POST.get("desc_tec")
+        try:
+            cursor = connection.cursor()
+            cursor.callproc("RH_AGREGAR_COMP_TEC_HAB_D", ["erick@sigssmac.com.mx", puesto, "Competencia", comp_tec,  desc_tec, int(dom_tec)])
+            mensaje = cursor.fetchall()[0][0]
+            cursor.close()
+        except IntegrityError as e:
+            print(e.message)
+            return HttpResponse(e.message, status=500)
+        if mensaje == "COMPETENCIA TECNICA/HABILIDAD AGREGADA CORRECTAMENTE":
+            return JsonResponse({"msg": mensaje, "tipo_res": "success"}, status=200)
+        else:
+            return JsonResponse({"msg": mensaje, "tipo_res": "error"}, status=500)
+
+def funcion7(request):
+    if request.method == 'POST':
+        puesto = request.POST.get("puesto")
+        habilidad = request.POST.get("habilidad")
+        desc_hab = request.POST.get("desc_hab")
+        dom_hab = request.POST.get("sl_dominio")
+        try:
+            cursor = connection.cursor()
+            cursor.callproc("RH_AGREGAR_COMP_TEC_HAB_D", ["erick@sigssmac.com.mx", puesto, "Habilidad", habilidad, desc_hab, dom_hab])
+            mensaje = cursor.fetchall()[0][0]
+            cursor.close()
+        except IntegrityError as e:
+            print(e.message)
+            return HttpResponse(e.message, status=500)
+        if mensaje == "COMPETENCIA TECNICA/HABILIDAD AGREGADA CORRECTAMENTE":
+            return JsonResponse({"msg": mensaje, "tipo_res": "success"}, status=200)
+        else:
+            return JsonResponse({"msg": mensaje, "tipo_res": "error"}, status=500)
+
+def funcion8(request):
+    if request.method == 'POST':
+        puesto = request.POST.get("puesto")
+        area_trab = request.POST.get("area_trab")
+        peligro = request.POST.get("peligro")
+        riesgo = request.POST.get("riesgo")
+        nivel = request.POST.get("nivel")
+        epp = request.POST.get("epp")
+        try:
+            cursor = connection.cursor()
+            cursor.callproc("RH_AGREGAR_ASP_SSMAC", ["erick@sigssmac.com.mx", puesto, area_trab, peligro, riesgo, nivel, epp])
+            mensaje = cursor.fetchall()[0][0]
+            cursor.close()
+        except IntegrityError as e:
+            print(e.message)
+            return HttpResponse(e.message, status=500)
+        if mensaje == "ASPECTO SSMAC AGREGADO":
+            return JsonResponse({"msg": mensaje, "tipo_res": "success"}, status=200)
+        else:
+            return JsonResponse({"msg": mensaje, "tipo_res": "error"}, status=500)
+
+def funcion9(request):
+    if request.method == 'POST':
+        puesto = request.POST.get("puesto")
+        sl_tipo_esf = request.POST.get("sl_tipo_esf")
+        desc_esf = request.POST.get("desc_esf")
+        try:
+            cursor = connection.cursor()
+            cursor.callproc("RH_AGREGAR_REQ_FISICOS", ["erick@sigssmac.com.mx", puesto, sl_tipo_esf, desc_esf])
+            mensaje = cursor.fetchall()[0][0]
+            cursor.close()
+            if mensaje == "REQUERIMIENTO FISICO AGREGADO CORRECTAMENTE":
+                return JsonResponse({"msg": mensaje, "tipo_res": "success"}, status=200)
+            else:
+                return JsonResponse({"msg": "Datos no almacenados", "tipo_res": "error"}, status=500)
+        except IntegrityError as e:
+            print(e.message)
+            return HttpResponse(e.message, status=500)
+
+
 def crear_directorio(request):
     if request.method == 'POST' and request.is_ajax():
         mensaje = ""
