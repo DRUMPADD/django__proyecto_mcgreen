34650846e45f7052d590af72cb9bc8f5369556a7
Erick <erickfco1999m@gmail.com>
2022-02-23 20:01:22 -0600

    crear perfil y directorio, y vistas

--- app/RR_HH/vistas.py
+++ app/RR_HH/vistas.py
@@ -1,20 +1,45 @@
+from audioop import reverse
 from django.shortcuts import redirect, render
+from django.db import OperationalError, connection
 
-
-def directorio(request):
+def directorio_perfil(request):
     if request.session.get("email"):
+        try:
+            cursor = connection.cursor()
+            puestos = connection.cursor()
+            puestos = cursor.execute("SELECT * from app_puestos")
+            puestos = cursor.fetchall()
+        except OperationalError as oe:
+            print("Error de operación en la base")
+            return False
+        finally:
+            cursor.close()
+        try:
+            cursor = connection.cursor()
+            departamentos = connection.cursor()
+            departamentos = cursor.execute("SELECT * from app_departamento")
+            departamentos = cursor.fetchall()
+        except OperationalError as oe:
+            print("Error de operación en la base")
+            return False
+        finally:
+            cursor.close()
+        try:
+            cursor = connection.cursor()
+            empleados = connection.cursor()
+            empleados = cursor.execute("SELECT * from app_empleados")
+            empleados = cursor.fetchall()
+        except OperationalError as oe:
+            print("Error de operación en la base")
+            return False
+        finally:
+            cursor.close()
         context = {
-            'privilegio': request.session.get("privilegio")
+            'privilegio': request.session.get("privilegio"),
+            'puestos': puestos,
+            'departamentos': departamentos,
+            'empleados': empleados,
         }
-        return render(request, 'RRHH/directorio.html', context)
+        return render(request, 'RRHH/directorio_perfil.html', context)
     else:
-        return redirect("/cerrar_sesion")
-
-def perfil_puesto(request):
-    if request.session.get("email"):
-        context = {
-            'privilegio': request.session.get("privilegio")
-        }
-        return render(request, 'RRHH/perfil_puesto.html', context)
-    else:
-        return redirect("/cerrar_sesion")
+        return redirect(reverse("/cerrar_sesion"))
