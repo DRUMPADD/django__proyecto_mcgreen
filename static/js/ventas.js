758f29c070d2bf5ae6b087d89adcbce042e07123
Erick <erickfco1999m@gmail.com>
2022-04-27 11:29:23 -0500

    registro de venta js

---
+++ static/js/ventas.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $("#form-c_p_c select[name='sl_sistemas']").on('change', function() {
+        var $this = $(this).val().split(' ')[1];
+        
+        $('#precio_usd').val(parseFloat($this));
+    });
+
+    $("form").submit(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            type: 'POST',
+            url: $("#btn_venta").attr("target-ajax"),
+            data: $("form").serializeArray(),
+            success: function(response) {
+                switch(response.status) {
+                    case 'success':
+                        swal.fire({
+                            position: 'center',
+                            icon: response.status,
+                            title: response.mensaje,
+                            showConfirmButton: false,
+                            timer: 4000
+                        })
+                        break;
+                    default:
+                        swal.fire({
+                            position: 'center',
+                            icon: response.status,
+                            title: response.mensaje,
+                            showConfirmButton: false,
+                            timer: 4000
+                        })
+                        break;
+                }
+            }
+        });
+    });
+});
