69830f94efdfff3c2412830fbd802bbb7b166a4c
Erick <erickfco1999m@gmail.com>
2022-04-27 13:09:16 -0500

    tomar datos de tabla

--- static/js/actualizar.js
+++ static/js/actualizar.js
@@ -10,15 +10,8 @@ $(document).ready(function() {
         
 
         $("input[name='id_']").val($(element).find("td").eq(0).text());
-        $("input[name='status']").val($(element).find("td").eq(1).text());
-        $("input[name='fecha_pago_fac']").val($(element).find("td").eq(2).text());
-        $("input[name='contrarecibo']").val($(element).find("td").eq(3).text());
-        $("input[name='fecha_rec_pago']").val($(element).find("td").eq(4).text());
-        $("input[name='fecha_de_fac']").val($(element).find("td").eq(18).text());
-        $("input[name='recibo_pago_fac_mcgreen']").val($(element).find("td").eq(19).text());
-        $("input[name='fecha_r_pag']").val($(element).find("td").eq(20).text());
-        $("input[name='dolar']").val(parseFloat($(element).find("td").eq(21).text().replace(',', '')));
-        $("input[name='monto_mn_pagado']").val(parseFloat($(element).find("td").eq(23).text().replace(',', '')));
+        $("input[name='fecha_registro']").val($(element).find("td").eq(1).text());
+        $("input[name='pozo']").val($(element).find("td").eq(3).text());
         e.preventDefault();
     });
 
