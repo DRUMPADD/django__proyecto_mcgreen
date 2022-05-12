01e773477b9d0bf4d8fe1c2f3ac39ae724dc7884
Erick <erickfco1999m@gmail.com>
2022-05-12 12:46:45 -0500

    funcion ajax envio de datos act. actividad

--- static/js/eventos.js
+++ static/js/eventos.js
@@ -32,4 +32,36 @@ $(document).ready(function() {
 
         e.preventDefault();
     });
+
+    $("form").each(function(index) {
+        var this_ = $(this);
+        this_.submit(function (e) {
+            e.preventDefault();
+            $.ajax({
+                type: "POST",
+                url: $("button").attr("target"),
+                data: this_.serializeArray(),
+                success: async function (res) {
+                    if(res.status == 'success') {
+                        await swal.fire({
+                            position: 'center',
+                            icon: res.status,
+                            title: res.msg,
+                            showConfirmButton: false,
+                            timer: 2000
+                        })
+                        location.reload(true);
+                    } else {
+                        swal.fire({
+                            position: 'center',
+                            icon: res.status,
+                            title: res.msg,
+                            showConfirmButton: false,
+                            timer: 2000
+                        })
+                    }
+                }
+            });
+        });
+    });
 });
