509c0ab5fe89d1783b1d3894c88960a141e3ac59
Erick <erickfco1999m@gmail.com>
2022-05-24 15:02:15 -0500

    agregando permisos

--- app/sigssmac/vistas.py
+++ app/sigssmac/vistas.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.db import connection, IntegrityError, InternalError, OperationalError
 
 def sigssmac_vista(request):
-    context = {}
-    try:
-        cursor = connection.cursor()
-        cursor.callproc("SIGSSMAC_REPORTES")
-        context["detalles"] = cursor.fetchall()
-    except (OperationalError, IntegrityError, InternalError) as e:
-        print(e)
-        return render(request, "errors/error500.html", {
-            "mensaje": "Contacte con el servicio de sistemas"
-        })
-    finally:
-        cursor.close()
-    return render(request, "sigssmac.html", context)
+    if request.session.get("email"):
+        context = {
+            'email': request.session.get("email"),
+            'privilegio': request.session.get("privilegio"),
+        }
+        try:
+            cursor = connection.cursor()
+            cursor.callproc("SIGSSMAC_REPORTES")
+            context["detalles"] = cursor.fetchall()
+        except (OperationalError, IntegrityError, InternalError) as e:
+            print(e)
+            return render(request, "errors/error500.html", {
+                "mensaje": "Contacte con el servicio de sistemas"
+            })
+        finally:
+            cursor.close()
+        return render(request, "sigssmac.html", context)
+    else:
+        return redirect("/cerrar_sesion")
