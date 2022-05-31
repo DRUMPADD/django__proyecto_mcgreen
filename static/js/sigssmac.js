f3125696b009c075c80449baac4986f709727e8c
Erick <erickfco1999m@gmail.com>
2022-05-31 12:01:35 -0500

    envio de formulario y muestra de imagenes

--- static/js/sigssmac.js
+++ static/js/sigssmac.js
@@ -1,26 +1,46 @@
 $(document).ready(function () {
-    $(".ver_img").each(function(index) {
-        var $this_ = $(this);
-        $this_.click(function () {
-            var div_display = $("#display_i");
-            var img_src = $("#img_mostrar");
-            img_src.attr("src", $this_.attr("src-img"));
-            img_src.show();
-            div_display.css("display", "block");
-        });
+    $("input[name='fecha_compromiso']").hide();
+    $("#btnVolver").hide();
+
+    $("select[name='sl_fecha']").change(function() {
+        if($(this).val() == 'fecha') {
+            $("input[name='fecha_compromiso']").toggle();
+            $("input[name='fecha_compromiso']").removeAttr("type");
+            $("input[name='fecha_compromiso']").attr("type", "date");
+            $("select[name='sl_fecha']").hide();
+            $("#btnVolver").show();
+        }
+        if($(this).val() == 'INMEDIATA') {
+            $("input[name='fecha_compromiso']").removeAttr("type");
+            $("input[name='fecha_compromiso']").attr("type", "text");
+            $("input[name='fecha_compromiso']").val('INMEDIATA');
+            $("#btnVolver").show();
+        }
     });
 
-    $(".close").each(function () {
-        var $this_ = $(this);
-        $this_.click(function () {
-            $this_.parent().parent().hide();
-        });
+    $("#btnVolver").click(() => {
+        $("select[name='sl_fecha']").prop('selectedIndex', 0);
+        $("select[name='sl_fecha']").show();
+        $("#btnVolver").toggle();
+        if($("input[name='fecha_compromiso']").show()) {
+            $("input[name='fecha_compromiso']").toggle();
+        }
     });
-    
     $("form").submit(function (e) {
         e.preventDefault();
-        var cont_inputs = $('input').filter(function(){return $(this).val() == ''}).length;
-        if(cont_inputs == 0 && $("textarea").val().trim() != '' && $("input[name='hallazgos']").val() != '') {
+        var cont_inputs = $('input').filter(function(){
+            var this_i = $(this);
+            if(this_i.attr("name") != 'herramientas') {
+                return this_i.val() == '';
+            }
+        }).length;
+        var selects = $('select').filter(function(){
+            var this_sl = $(this);
+            if(this_sl.attr("name") != 'sl_afecta') {
+                return this_sl.val() == null;
+            }
+        }).length;
+        if(cont_inputs == 0 && $("textarea").val().trim() != '' && selects == 0 && $("input[name='hallazgos']").val() != '') {
             if(window.FormData !== undefined) {
                 var formData = new FormData(this);
                 var xhr = new XMLHttpRequest();
@@ -30,7 +50,9 @@ $(document).ready(function () {
                     if(xhr.readyState == 4) {
                         if(xhr.status == 200) {
                             res = JSON.parse(xhr.responseText);
+                            // Code for success upload
                             if(res.status == 'success') {
+                                // await alert("Resultado: " + res.msg);
                                 await swal.fire({
                                     position: 'center',
                                     icon: res.status,
@@ -38,9 +60,10 @@ $(document).ready(function () {
                                     showConfirmButton: false,
                                     timer: 3000
                                 })
-                                await $("form").trigger("reset");
+                                $("form").trigger("reset");
                                 location.reload(true);
                             } else {
+                                // await alert("Resultado: " + res.msg);
                                 await swal.fire({
                                     position: 'center',
                                     icon: res.status,
@@ -51,25 +74,14 @@ $(document).ready(function () {
                             }
                         }
                         else {
-                            swal.fire({
-                                position: 'center',
-                                icon: "error",
-                                title: "Error al enviar la información",
-                                showConfirmButton: false,
-                                timer: 3000
-                            })
+                            console.log("No se pudo");
+                            // Code for error
                         }
                     }
                 };
                 xhr.send(formData);
             } else {
-                swal.fire({
-                    position: 'center',
-                    icon: "error",
-                    title: "La información no pudo ser enviada",
-                    showConfirmButton: false,
-                    timer: 3000
-                })
+                console.log("No existe");
             }
         } else {
             swal.fire({
@@ -80,5 +92,35 @@ $(document).ready(function () {
                 timer: 3000
             })
         }
+    });
+
+    $(".ver_img").each(function(index) {
+        var $this_ = $(this);
+        $this_.click(async function () {
+            var div_display = $("#display_i");
+            var img_src = $("#img_mostrar");
+            let src_ = $this_.attr("src-img");
+            if(src_ == 'media/img/None') {
+                await swal.fire({
+                    position: 'center',
+                    icon: "error",
+                    title: "No existe imagen",
+                    showConfirmButton: false,
+                    timer: 3000
+                })
+            } else {
+                img_src.attr("src", src_);
+                await img_src.show();
+                await div_display.css("display", "block");
+            }
+
+        });
+    });
+    
+    $(".close").each(function () {
+        var $this_ = $(this);
+        $this_.click(function () {
+            $this_.parent().parent().hide();
+        });
     });
 });
