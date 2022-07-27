b8c0d1900a49cd53f901e42903a0f0484483d663
Erick <erickfco1999m@gmail.com>
2022-07-27 18:30:23 -0500

    validando si se recibe dato de cada variable

--- app/sigssmac/posts.py
+++ app/sigssmac/posts.py
@@ -2,21 +2,21 @@ from django.db import connection, OperationalError, IntegrityError, InternalErro
 from django.http import JsonResponse
 
 def sigssmac_post(request):
-    fecha_inicio = request.POST.get("fecha_inicio")
-    fecha_compromiso = request.POST.get("fecha_compromiso")
-    fecha_cierre = request.POST.get("fecha_cierre")
-    indice_actos = request.POST.get("indice_actos")
-    origen = request.POST.get("sl_origen")
+    fecha_inicio = request.POST.get("fecha_inicio") if request.POST.get("fecha_inicio") != None or request.POST.get("fecha_inicio") != '' else ''
+    fecha_compromiso = request.POST.get("fecha_compromiso") if request.POST.get("fecha_compromiso") != None or request.POST.get("fecha_compromiso") != '' else ''
+    fecha_cierre = request.POST.get("fecha_cierre") if request.POST.get("fecha_cierre") != None or request.POST.get("fecha_cierre") != '' else ''
+    indice_actos = request.POST.get("indice_actos") if request.POST.get("indice_actos") != None or request.POST.get("indice_actos") != '' else 0
+    origen = request.POST.get("sl_origen") if request.POST.get("sl_origen") != None or request.POST.get("sl_origen") != '' else 'N/A'
     herramientas = request.POST.get("herramientas") if request.POST.get("herramientas") != None else 'N/A'
-    tipo_i = request.POST.get("sl_tipo_i")
-    acto = request.POST.get("sl_acto")
+    tipo_i = request.POST.get("sl_tipo_i") if request.POST.get("sl_tipo_i") != None or request.POST.get("sl_tipo_i") != '' else 'N/A'
+    acto = request.POST.get("sl_acto") if request.POST.get("sl_acto") != None or request.POST.get("sl_acto") != '' else 'N/A'
     afecta = request.POST.get("sl_afecta") if request.POST.get("sl_afecta") != None or request.POST.get("sl_afecta") != '' else 'N/A'
-    categoria = request.POST.get("categoria")
-    hallazgo_obser = request.POST.get("hallazgo_obser")
-    accion = request.POST.get("accion")
-    responsable = request.POST.get("sl_responsable")
-    prioridad = request.POST.get("sl_prioridad")
-    estatus = request.POST.get("sl_status")
+    categoria = request.POST.get("categoria") if request.POST.get("categoria") != None or request.POST.get("categoria") != '' else 'N/A'
+    hallazgo_obser = request.POST.get("hallazgo_obser") if request.POST.get("hallazgo_obser") != None or request.POST.get("hallazgo_obser") != '' else 'N/A'
+    accion = request.POST.get("accion") if request.POST.get("accion") != None or request.POST.get("accion") != '' else 'N/A'
+    responsable = request.POST.get("sl_responsable") if request.POST.get("sl_responsable") != None or request.POST.get("sl_responsable") != '' else 'N/A'
+    prioridad = request.POST.get("sl_prioridad") if request.POST.get("sl_prioridad") != None or request.POST.get("sl_prioridad") != '' else 'N/A'
+    estatus = request.POST.get("sl_status") if request.POST.get("sl_status") != None or request.POST.get("sl_status") != '' else 'N/A'
     hallazgos = request.FILES.get("hallazgos")
     
     print(fecha_inicio)
