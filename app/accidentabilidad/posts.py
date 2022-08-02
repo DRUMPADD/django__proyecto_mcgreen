205c88c03f22f0edd5e7ec70fe6904cb194ca1ff
Erick <erickfco1999m@gmail.com>
2022-08-02 16:06:27 -0500

    vistas y peticion post para accidentabilidad

---
+++ app/accidentabilidad/posts.py
@@ -0,0 +1,17 @@
+from django.http import JsonResponse
+from django.db import InternalError, OperationalError, ProgrammingError, connection
+import json
+
+def accidentabilidad_post(request):
+    if request.method == 'POST':
+        res = json.loads(request.body.decode('utf-8'))
+        mensaje = ""
+        try:
+            cursor = connection.cursor()
+            cursor.callproc("ACCIDENTABILIDAD", [request.session.get("email"), res["c_personal"][0], res["h_trabajo"][0], res["jornada"][0], res["c_personal"][1], res["h_trabajo"][1], res["jornada"][1]])
+            mensaje = cursor.fetchall()[0][0]
+
+            return JsonResponse({"status": "success", "res": mensaje}, status=200)
+        except (OperationalError, InternalError, ProgrammingError) as e:
+            print(e)
+            return JsonResponse({"status": "error", "res": "Ocurrió un error en el sistema"}, status=200)
---
+++ app/accidentabilidad/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.db import InterfaceError, OperationalError, ProgrammingError, connection
+
+def accidentabilidad_vista(request):
+    context = {
+        "saludo": "Accidentabilidad"
+    }
+    try:
+        cursor = connection.cursor()
+        cursor.execute("select * from app_personal_propio")
+        context["personal_propio"] = cursor.fetchall()
+    except (OperationalError, InterfaceError, ProgrammingError) as e:
+        print(e)
+        return render(request, "errors/error500.html", {
+            "mensaje": "Contacte con el servicio de sistemas"
+        })
+    try:
+        cursor = connection.cursor()
+        cursor.execute("select * from app_personal_con")
+        context["personal_contratado"] = cursor.fetchall()
+    except (OperationalError, InterfaceError, ProgrammingError) as e:
+        print(e)
+        return render(request, "errors/error500.html", {
+            "mensaje": "Contacte con el servicio de sistemas"
+        })
+    return render(request, "RRHH/accidentabilidad.html", context)
