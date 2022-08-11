84e81d74d9d8dac6756074bc7b0a7f65c8f9a176
Erick <erickfco1999m@gmail.com>
2022-08-11 00:38:12 -0500

    agregando nuevos parametros para insertar desde en el servidor

--- app/accidentabilidad/posts.py
+++ app/accidentabilidad/posts.py
@@ -8,10 +8,10 @@ def accidentabilidad_post(request):
         mensaje = ""
         try:
             cursor = connection.cursor()
-            cursor.callproc("ACCIDENTABILIDAD", [request.session.get("email"), res["c_personal"][0], res["h_trabajo"][0], res["jornada"][0], res["c_personal"][1], res["h_trabajo"][1], res["jornada"][1]])
+            cursor.callproc("ACCIDENTABILIDAD", [request.session.get("email"), res["c_personal"][0], res["h_trabajo"][0], res["jornada"][0], res["c_personal"][1], res["h_trabajo"][1], res["jornada"][1]], res["mes"], res["anio"])
             mensaje = cursor.fetchall()[0]
 
-            return JsonResponse({"status": "success", "res": mensaje}, status=200)
+            return JsonResponse({"status": "success", "res": mensaje}, status=200),
         except (OperationalError, InternalError, ProgrammingError) as e:
             print(e)
             return JsonResponse({"status": "error", "res": "Ocurrió un error en el sistema"}, status=200)
