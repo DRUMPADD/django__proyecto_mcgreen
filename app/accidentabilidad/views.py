9bd4d66b51f2d4f88b6b3bbfffa3a74979687dac
Erick <erickfco1999m@gmail.com>
2022-08-16 12:58:46 -0500

    quitando funcion para obtener datos de empleados propios y subcontratados

--- app/accidentabilidad/views.py
+++ app/accidentabilidad/views.py
@@ -1,42 +1,44 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db import InterfaceError, InternalError, OperationalError, ProgrammingError, connection
 
 def accidentabilidad_vista(request):
-    context = {
-        "saludo": "Accidentabilidad",
-        'email': request.session.get("email"),
-        'privilegio': request.session.get("privilegio"),
-    }
-    try:
-        cursor = connection.cursor()
-        cursor.execute("select * from app_personal_propio")
-        context["personal_propio"] = cursor.fetchall()
-    except (OperationalError, InterfaceError, ProgrammingError) as e:
-        print(e)
-        return render(request, "errors/error500.html", {
-            "mensaje": "Contacte con el servicio de sistemas"
-        })
-    try:
-        cursor = connection.cursor()
-        cursor.execute("select * from app_personal_con")
-        context["personal_contratado"] = cursor.fetchall()
-    except (OperationalError, InterfaceError, ProgrammingError) as e:
-        print(e)
-        return render(request, "errors/error500.html", {
-            "mensaje": "Contacte con el servicio de sistemas"
-        })
-    try:
-        cursor = connection.cursor()
-        cursor.callproc("DETALLES_ACCIDENTABILIDAD")
-        context["detalles_acci"] = cursor.fetchall()
-    except (OperationalError, InterfaceError, ProgrammingError) as e:
-        print(e)
-        return render(request, "errors/error500.html", {
-            "mensaje": "Contacte con el servicio de sistemas"
-        })
-    return render(request, "RRHH/accidentabilidad.html", context)
-
+    if request.session.get("email"):
+        context = {
+            "saludo": "Accidentabilidad",
+            'email': request.session.get("email"),
+            'privilegio': request.session.get("privilegio"),
+        }
+        try:
+            cursor = connection.cursor()
+            cursor.execute("select * from app_personal_propio")
+            context["personal_propio"] = cursor.fetchall()
+        except (OperationalError, InterfaceError, ProgrammingError) as e:
+            print(e)
+            return render(request, "errors/error500.html", {
+                "mensaje": "Contacte con el servicio de sistemas"
+            })
+        try:
+            cursor = connection.cursor()
+            cursor.execute("select * from app_personal_con")
+            context["personal_contratado"] = cursor.fetchall()
+        except (OperationalError, InterfaceError, ProgrammingError) as e:
+            print(e)
+            return render(request, "errors/error500.html", {
+                "mensaje": "Contacte con el servicio de sistemas"
+            })
+        try:
+            cursor = connection.cursor()
+            cursor.callproc("DETALLES_ACCIDENTABILIDAD")
+            context["detalles_acci"] = cursor.fetchall()
+        except (OperationalError, InterfaceError, ProgrammingError) as e:
+            print(e)
+            return render(request, "errors/error500.html", {
+                "mensaje": "Contacte con el servicio de sistemas"
+            })
+        return render(request, "RRHH/accidentabilidad.html", context)
+    else:
+        return redirect("/cerrar_sesion")
 
 
 def datos_x_meses(request):
@@ -51,21 +53,4 @@ def datos_x_meses(request):
         return JsonResponse({"respuesta": mensaje}, status=200) 
     except (OperationalError, InternalError, ProgrammingError) as e:
         print(e)
-
-def accidentabilidad_res_totales(request):
-    mensaje = ""
-    try:
-        cursor = connection.cursor()
-        cursor2 = connection.cursor()
-        cursor.execute("select mes_subcon, sum(cant_con_p), sum(total_calculo) from app_personal_con group by mes_subcon")
-        cursor2.execute("select mes_propio, sum(cant_pro_p), sum(total_calculo) from app_personal_propio group by mes_propio")
-        mensaje = cursor.fetchall()
-        mensaje2 = cursor2.fetchall()
-        
-        print(mensaje)
-        print(mensaje2)
-        for pro in mensaje:
-            print(pro)
-        return JsonResponse({"propios": mensaje}, status=200) 
-    except (OperationalError, InternalError, ProgrammingError) as e:
-        print(e)
+        return JsonResponse({"respuesta": "Error en el sistema"}, status=200)
