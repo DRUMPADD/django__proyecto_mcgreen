4965a1eefbcd8d63c6924499cdafa94b9a39ed44
Erick Olvera <erickfco1999@gmail.com>
2022-11-07 17:30:14 -0600

    Ruta para conexion a BD

---
+++ routers/routers.py
@@ -0,0 +1,25 @@
+class McGreenRouter:
+    route_app_labels = {'mcgreen_plataforma'}
+
+    def db_for_read(self, model, **hints):
+        if model._meta.app_label in self.route_app_labels:
+            return 'mcgreen_conexion'
+        return None
+
+    def db_for_write(self, model, **hints):
+        if model._meta.app_label in self.route_app_labels:
+            return 'mcgreen_conexion'
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        if (
+            obj1._meta.app_label in self.route_app_labels or
+            obj2._meta.app_label in self.route_app_labels
+        ):
+            return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if app_label in self.route_app_labels:
+            return db == 'mcgreen_conexion'
+        return None
